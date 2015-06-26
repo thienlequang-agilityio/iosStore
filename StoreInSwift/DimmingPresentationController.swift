08de2e42e85620730365f722997a2e088d92f9a9
thienlequang <quangthien@outlook.com>
2015-06-26 10:34:49 +0700

    add animation

--- StoreInSwift/DimmingPresentationController.swift
+++ StoreInSwift/DimmingPresentationController.swift
@@ -14,6 +14,22 @@ class DimmingPresentationController: UIPresentationController {
     override func presentationTransitionWillBegin() {
         dimmingView.frame = containerView.bounds
         containerView.insertSubview(dimmingView, atIndex: 0)
+        
+        dimmingView.alpha = 0
+        
+        if let transitionCoordinator = presentedViewController.transitionCoordinator() {
+            transitionCoordinator.animateAlongsideTransition({ (_) -> Void in
+                self.dimmingView.alpha = 1
+            }, completion: nil)
+        }
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        if let transitionCoordinator = presentedViewController.transitionCoordinator() {
+            transitionCoordinator.animateAlongsideTransition({ (_) -> Void in
+                self.dimmingView.alpha = 0
+                }, completion: nil)
+        }
     }
     override func shouldRemovePresentersView() -> Bool {
         return false
--- StoreInSwift/ViewControllers/DetailViewController.swift
+++ StoreInSwift/ViewControllers/DetailViewController.swift
@@ -95,4 +95,8 @@ extension DetailViewController: UIViewControllerTransitioningDelegate {
     func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController!, sourceViewController source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presentingViewController: presenting)
     }
+    
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BounceAnimationController()
+    }
 }
